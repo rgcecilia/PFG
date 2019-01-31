32de6f68523aec1d45757c7aaa5ae1e8959b3488
rgcec <rgcec@RGC-AY135NS>
2019-01-31 22:31:38 +0100

    Entorno Produccion

--- dao/contenido_dao.php
+++ dao/contenido_dao.php
@@ -11,29 +11,30 @@ if(isset($_POST['inicio'])){
         $resultado=mysqli_query($con, $consulta);
         $_SESSION['mensaje'] = "Inicio Actualizado";
         mysqli_close($con);
-        header("location:../administradores.php"); 
-    }           
-    if($_FILES['imagen']['size']>1){
+        header("refresh:0, url=../administradores.php"); 
+    }
+   if($_FILES['imagen']['size']>1){
         $nombre_img = $_FILES['imagen']['name'];
         $tipo = $_FILES['imagen']['type'];
         $tamano = $_FILES['imagen']['size'];
-        if (($nombre_img == !NULL) && ($_FILES['imagen']['size'] <= 1500000)){
+        if (($nombre_img == !NULL) && ($_FILES['imageninicio']['size'] <= 1500000)){
+            //indicamos los formatos que permitimos subir a nuestro servidor
             if (($_FILES["imagen"]["type"] == "image/jpeg") || ($_FILES["imagen"]["type"] == "image/jpg") || ($_FILES["imagen"]["type"] == "image/png")){        
                 $uploads_dir = "/kunden/homepages/4/d769464405/htdocs/img/inicio";
                 move_uploaded_file($_FILES['imagen']['tmp_name'],"$uploads_dir/$nombre_img");
             }else{
                 $_SESSION['mensaje'] = "Formato de imagen de inicio invalido";
                 mysqli_close($con);
-                header("location:../administradores.php"); 
+                header("refresh:0, url=../administradores.php"); 
             }
         } 
         $titulo = $_POST['titulo'];
         $texto = $_POST['texto'];
         $consulta="update datos set titulo_inicio='".$titulo."', txt_inicio='".$texto."', img_inicio='img/inicio/".$nombre_img."';";
         $resultado=mysqli_query($con, $consulta);
-        $_SESSION['mensaje'] = "Inicio Actualizado";
+        $_SESSION['mensaje'] = "Inicio Actualizado con imagen";
         mysqli_close($con);
-        header("location:../administradores.php"); 
+        header("refresh:0, url=../administradores.php"); 
     }
 }
 if(isset($_POST['nosotros'])){
@@ -44,9 +45,8 @@ if(isset($_POST['nosotros'])){
         $resultado=mysqli_query($con, $consulta);
         $_SESSION['mensaje'] = "Sobre Nosotros Actualizado";
         mysqli_close($con);
-        header("location:../administradores.php"); 
-    }           
-    if($_FILES['imagen']['size']>1){
+        header("refresh:0, url=../administradores.php"); 
+    }else{
         $nombre_img = $_FILES['imagen']['name'];
         $tipo = $_FILES['imagen']['type'];
         $tamano = $_FILES['imagen']['size'];
@@ -57,16 +57,16 @@ if(isset($_POST['nosotros'])){
             }else{
                 $_SESSION['mensaje'] = "Formato de imagen de seccion nosotros invalido";
                 mysqli_close($con);
-                header("location:../administradores.php"); 
+                header("refresh:0, url=../administradores.php"); 
             }
         } 
         $titulo = $_POST['titulo'];
         $texto = $_POST['texto'];
         $consulta="update datos set titulo_nosotros='".$titulo."', txt_nosotros='".$texto."', img_nosotros='img/sobre/".$nombre_img."';";
         $resultado=mysqli_query($con, $consulta);
-        $_SESSION['mensaje'] = "Seccion Sobre Nosotros Actualizada";
+        $_SESSION['mensaje'] = "Seccion Sobre Nosotros Actualizada con imagen";
         mysqli_close($con);
-        header("location:../administradores.php"); 
+        header("refresh:0, url=../administradores.php"); 
     }   
 }
 ?>
