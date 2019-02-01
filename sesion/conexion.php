d6fe07ac3a979c8443ac3e3f7556a92b24e99479
rgcec <rgcec@RGC-AY135NS>
2019-02-01 10:55:13 +0100

    Entorno Produccion

--- sesion/conexion.php
+++ sesion/conexion.php
@@ -4,4 +4,10 @@
   $pass = 'Linkia@2019';
   $db_name = 'db769464522';
   $con = mysqli_connect($host, $user, $pass, $db_name) or die("Error al conectar con la base de datos");
+  
+//    $host = 'localhost';
+//    $user = 'root';
+//    $pass = '12345';
+//    $db_name = 'procuradores';
+//    $con = mysqli_connect($host, $user, $pass, $db_name) or die("Error al conectar con la base de datos");
 ?>
